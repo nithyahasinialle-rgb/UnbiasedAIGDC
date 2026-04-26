27434800ecd7cd91cf4a1cc8e3fc092feb9a9f8b
Nithyahasini15 <24071a1067@vnrvjiet.in>
2026-04-27 01:39:53 +0530

    fix api url and file persistence

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 // src/main.jsx
+import { wakeUpBackend } from './api/client'
+wakeUpBackend()
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
